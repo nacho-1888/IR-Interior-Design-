699948a10fe85a152b51fe4614f0a080d81986ab
Ignacio Lozano <nacho@Ignacios-MacBook-Air.local>
2026-03-30 18:47:48 +0700

    Refine: Reverted to a sleek, blocky geometric font (Montserrat) for a monolithic architectural look

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar() {
             >
               <Link 
                 to="/" 
-                className="text-6xl md:text-[8rem] lg:text-[10rem] branding-font font-normal leading-none tracking-tight block whitespace-nowrap uppercase text-white"
+                className="text-6xl md:text-[8rem] lg:text-[10rem] branding-font font-bold leading-none tracking-tight block whitespace-nowrap uppercase text-white"
               >
                 Isabel Romer
               </Link>
