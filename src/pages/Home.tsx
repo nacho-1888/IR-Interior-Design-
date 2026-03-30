186aa3a53f15abcaea0a68f8c9a195e47fee8875
Ignacio Lozano <nacho@Ignacios-MacBook-Air.local>
2026-03-30 21:26:16 +0700

    Refine: Repositioned the hero project title to a lower, asymmetrical 'off-center' position for better architectural balance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -97,7 +97,7 @@ export default function Home({ onContactOpen }: { onContactOpen: () => void }) {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.5, duration: 1 }}
-              className="max-w-[280px] md:max-w-xl absolute bottom-8 left-0 md:left-2"
+              className="max-w-[280px] md:max-w-xl absolute bottom-6 left-4 md:left-[10%]"
             >
               <AnimatePresence mode="wait">
                 <motion.h2
